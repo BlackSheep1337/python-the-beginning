dc984e99aa4d74e9dba24a2bff7c873d96203f96
Ale Pereira <naitwa-alexandre@gmail.com>
2025-01-14 20:44:50 -0300

    first class

---
+++ pratice01/variables/class01.py
@@ -0,0 +1,10 @@
+age = 20
+
+price = 19.95
+
+first_name = "Alzandar"
+
+is_online = True
+
+print({ age, first_name, price, is_online })
+
---
+++ pratice01/variables/class03.py
@@ -0,0 +1,6 @@
+first_number = float(input("Digit your first number: "))
+second_number = float(input("Digit your second number: "))
+
+total = second_number + first_number
+
+print("The total is: " + str(total))
